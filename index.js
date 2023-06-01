03e1f2885b6c9ee2033ac4d5e8e7b01de76e0c90
Waseem Ullah <106027684+waseem1903@users.noreply.github.com>
2023-06-01 17:48:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://chat-app-front-end-zeta.vercel.app",
     methods: ["GET", "POST"],
     allowedHeaders: ["Access-Control-Allow-Origin"],
     credentials: true,
