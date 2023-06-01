87891927b6c87204cad4b60ad0afbf7cae8f9e4d
Waseem Ullah <106027684+waseem1903@users.noreply.github.com>
2023-06-01 14:52:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const cors = require("cors");
 const { Server } = require("socket.io");
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
